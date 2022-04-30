4f5c28b88c358cb23f288db9cd8d93daeda14377
Kholina Dasha <holina-dasha@mail.ru>
2022-04-30 14:45:44 +0300

    init commit

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../scss/App.scss';
+import Main from '../../pages/main/main';
+
+const App = () => {
+  return (
+    <Main />
+  );
+}
+
+export default App;
