d883c178864ec3a7293e64f764b3156eb5f0780b
Kholina Dasha <holina-dasha@mail.ru>
2022-05-23 14:51:19 +0300

    deploy

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ export const api = {
 
     // Задачи
     getTasks: async (filter, page, limit) => {
-        const response = await inst.post('/tasks', {
+        const response = await inst.post('http://93.95.97.34/api/tasks', {
             filter: filter,
             page: page,
             limit: limit
@@ -18,7 +18,7 @@ export const api = {
     },
 
     addTask: async (data) => {
-        await inst.put('/tasks/createOrEdit', {
+        await inst.put('http://93.95.97.34/api/tasks/createOrEdit', {
             ...data,
             dateOfUpdate: new Date(),
             timeInMinutes: 0,
@@ -26,50 +26,50 @@ export const api = {
     },
 
     deleteTask: async (taskId) => {
-        await inst.delete(`/tasks/${taskId}`)
+        await inst.delete(`http://93.95.97.34/api/tasks/${taskId}`)
     },
 
     editStatus: async (taskId, status) => {
-        await inst.patch(`/tasks/${taskId}/status/${status}`)
+        await inst.patch(`http://93.95.97.34/api/tasks/${taskId}/status/${status}`)
     },
 
     addWorktime: async (taskId, data) => {
-        await inst.patch(`/tasks/${taskId}/worktime`, data)
+        await inst.patch(`http://93.95.97.34/api/tasks/${taskId}/worktime`, data)
     },
 
     // Комментарии
     getComments: async (taskId) => {
-        const response = await inst.get(`/comments/${taskId}`)
+        const response = await inst.get(`http://93.95.97.34/api/comments/${taskId}`)
         return response.data;
     },
 
     addComments: async (data) => {
-        await inst.put(`/comments/createOrEdit`, data)
+        await inst.put(`http://93.95.97.34/api/comments/createOrEdit`, data)
     },
 
     deleteComment: async (commentId) => {
-        return await inst.delete(`/comments/${commentId}`)
+        return await inst.delete(`http://93.95.97.34/api/comments/${commentId}`)
     },
 
     // Пользователи
     getUsers: async () => {
-        const response = await inst.get(`/users/all`)
+        const response = await inst.get(`http://93.95.97.34/api/users/all`)
         return response;
     },
 
     editUser: async (data) => {
-        await inst.put('/users/edit', {
+        await inst.put('http://93.95.97.34/api/users/edit', {
             ...data,
         })
     },
 
     loginIn: async (login, password) => {
-            const response = await inst.post(`/users/login`, { "login": `${login}`, "password": `${password}` })
+            const response = await inst.post(`http://93.95.97.34/api/users/login`, { "login": `${login}`, "password": `${password}` })
             return response;
     },
 
     getLoggedUser: async (id) => {
-        const response = await inst.get(`/users/${id}`)
+        const response = await inst.get(`http://93.95.97.34/api/users/${id}`)
         return response;
     }
 }
